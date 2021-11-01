13238ac81cfd9f800f33e8ba63c09090229020c4
Ben Puckhaber <ben.puckhaber@gmail.com>
2021-11-01 15:20:01 -0500

    exercise.08-extra-02

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -5,19 +5,47 @@ import * as React from 'react'
 import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-test('exposes the count and increment/decrement functions', () => {
-  let counter
-  function CounterComponent() {
-    counter = useCounter()
+function setup({initialProps} = {}) {
+  let counter = {}
+  function CounterComponent(props) {
+    Object.assign(counter, useCounter(props))
     return null
   }
 
-  render(<CounterComponent />)
+  render(<CounterComponent {...initialProps} />)
+
+  return counter
+}
+
+test('exposes the count and increment/decrement functions', () => {
+  const counter = setup()
 
   expect(counter.count).toBe(0)
 
   act(() => counter.increment())
   expect(counter.count).toBe(1)
+
+  act(() => counter.decrement())
+  expect(counter.count).toBe(0)
+})
+
+test('allows customization of the initial count', () => {
+  const counter = setup({
+    initialProps: {initialCount: 3},
+  })
+
+  expect(counter.count).toBe(3)
+})
+
+test('allows customization of the step', () => {
+  const counter = setup({
+    initialProps: {step: 2},
+  })
+
+  expect(counter.count).toBe(0)
+
+  act(() => counter.increment())
+  expect(counter.count).toBe(2)
 
   act(() => counter.decrement())
   expect(counter.count).toBe(0)
